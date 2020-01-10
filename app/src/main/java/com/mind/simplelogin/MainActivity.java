6c9d2bc40594028c05c84f77bc59e87d8d79b13c
stelzer.l@icloud.com <Langweilig11111>
2020-01-10 16:37:02 +0100

    login

--- app/src/main/java/com/mind/simplelogin/MainActivity.java
+++ app/src/main/java/com/mind/simplelogin/MainActivity.java
@@ -3,6 +3,7 @@ package com.mind.simplelogin;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,34 +14,82 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button btRegister;
-    private ImageView circle1;
+     Button btRegister;
+     ImageView circle1;
     TextView tvLogin;
-    private Button login;
+     Button login;
+    EditText mEmail,mPassword;
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btRegister  = findViewById(R.id.btRegister);
-        tvLogin     = findViewById(R.id.tvLogin);
-        circle1     = findViewById(R.id.circle1);
-        login      = findViewById(R.id.btLogin);
+        btRegister = findViewById(R.id.btRegister);
+        tvLogin = findViewById(R.id.tvLogin);
+        circle1 = findViewById(R.id.circle1);
+        login = findViewById(R.id.btLogin);
+        mEmail = findViewById(R.id.etUsername);
+        mPassword = findViewById(R.id.etPassword);
 
         btRegister.setOnClickListener(this);
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Startseite.class);
-                startActivity(intent);
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Benutzername is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+
+
+                // authenticate the user
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
             }
         });
     }
-
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onClick(View v) {
--- app/src/main/java/com/mind/simplelogin/RegisterActivity.java
+++ app/src/main/java/com/mind/simplelogin/RegisterActivity.java
@@ -1,25 +1,65 @@
 package com.mind.simplelogin;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import io.opencensus.tags.Tag;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private RelativeLayout rlayout;
-    private Animation animation;
-    private Menu menu;
-    private Button login;
+    RelativeLayout rlayout;
+    Animation animation;
+    Menu menu;
+    Button login;
+    FirebaseAuth fAuth;
+    EditText eemail, ename, epassswort, ewpasswort;
+    ProgressBar progressBar;
+    String userID;
+    FirebaseFirestore fStore;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,13 +73,70 @@ public class RegisterActivity extends AppCompatActivity {
         animation   = AnimationUtils.loadAnimation(this,R.anim.uptodown);
         rlayout.setAnimation(animation);
         login      = findViewById(R.id.btLogin);
+        eemail      = findViewById(R.id.etEmail);
+        ename      = findViewById(R.id.etUsername);
+        epassswort      = findViewById(R.id.etPassword);
+        ewpasswort      = findViewById(R.id.etRePassword);
+
+
+        fAuth = FirebaseAuth.getInstance();
+        // progressBar = findViewById(R.id.progressBar);
+
 
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(RegisterActivity.this, Startseite.class);
-                startActivity(intent);
+                final String email = eemail.getText().toString().trim();
+                String password = epassswort.getText().toString().trim();
+                final String fullName = ename.getText().toString();
+                 String wpasswort    = ewpasswort.getText().toString();
+
+                if(TextUtils.isEmpty(email)){
+                    eemail.setError("Email is Required.");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    epassswort.setError("Password is Required.");
+                    return;
+                }
+
+                else if(TextUtils.isEmpty(fullName)){
+                    epassswort.setError("Benutzername muss gewählt sein");
+                    return;
+                }
+
+                else if(password.length() < 6){
+                    epassswort.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+                else if(password != wpasswort){
+                    ewpasswort.setError("Muss gleich sein");
+                    return;
+                }
+
+
+
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User Created.", Toast.LENGTH_SHORT).show();
+//                            DocumentReference documentReference = fStore.collection("users").document(userID);
+ //                           Map<String,Object> user = new HashMap<>();
+  //                          user.put("fName",fullName);
+   //                         user.put("email",email);
+
+
+                            startActivity(new Intent(getApplicationContext(),Startseite.class));
+
+                        }else {
+                            Toast.makeText(RegisterActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
             }
         });
     }
