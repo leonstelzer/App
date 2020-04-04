f4a0bf5042ceed11cd1ccfe7b9762a572d2fd122
stelz <stelzer.l@icloud.com>
2020-04-04 12:10:18 +0200

    benachrichtigung

--- app/src/main/java/com/mind/simplelogin/Beanchrichtigung.java
+++ app/src/main/java/com/mind/simplelogin/Beanchrichtigung.java
@@ -1,24 +1,34 @@
 package com.mind.simplelogin;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.firestore.SetOptions;
+import com.mind.simplelogin.Profil.otherProfile;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
@@ -44,8 +54,7 @@ public class Beanchrichtigung extends AppCompatActivity {
         mFirestore = FirebaseFirestore.getInstance();
         usersList = new ArrayList<>();
         benachrichtigungAdapter = new BenachrichtigungAdapter(getApplicationContext(), usersList);
-        String usid = fAuth.getCurrentUser().getUid();
-
+        final String usid = fAuth.getCurrentUser().getUid();
 
         friendlist.setHasFixedSize(true);
         friendlist.setLayoutManager(new LinearLayoutManager((this)));
@@ -60,13 +69,14 @@ public class Beanchrichtigung extends AppCompatActivity {
 
                                                                                                         }
                                                                                                         System.out.println("MAP GROEESSE : " + queryDocumentSnapshots.getDocumentChanges().size());
-                                                                                                        for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+
+                                                                                                         for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
                                                                                                             if (doc.getType() == DocumentChange.Type.ADDED) {
                                                                                                                 String type = (String) doc.getDocument().get("Type");
                                                                                                                 String otherid = (String) doc.getDocument().get("otherid");
 
                                                                                                                 System.out.println("type:"+type);
-                                                                                                                System.out.println("otherid:"+otherid);
+                                                                                                                System.out.println("otherid:"+ otherid);
 
                                                                                                                 final String otherID = (String) doc.getDocument().get("otherid");
                                                                                                                 if (type.equals("received")) {
@@ -83,6 +93,7 @@ public class Beanchrichtigung extends AppCompatActivity {
                                                                                                                                         Users users = doc.getDocument().toObject(Users.class).withId(otherID);
                                                                                                                                         usersList.add(users);
                                                                                                                                         benachrichtigungAdapter.notifyDataSetChanged();
+
                                                                                                                                     }
                                                                                                                                 }
                                                                                                                             }
@@ -93,9 +104,92 @@ public class Beanchrichtigung extends AppCompatActivity {
                                                                                                         }
                                                                                                     }
                                                                                                 });
-                        //To
         annehmen = findViewById(R.id.annehmen);
         ablehnen = findViewById(R.id.ablehnen);
+
+        ablehnen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fStore.collection("users").document(otherid).collection("request").document(otherid+usid).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Beanchrichtigung.this, "Gelöscht", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Beanchrichtigung.this, "Nö", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+                fStore.collection("users").document(usid).collection("request").document(usid+otherid).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(Beanchrichtigung.this, "Nö", Toast.LENGTH_SHORT).show();
+
+
+                    }
+                });
+            }
+
+        });
+
+        annehmen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Map<String, String> friends = new HashMap<>();
+                friends.put(otherid, otherid);
+
+
+                final Map<String, String> friends1 = new HashMap<>();
+                friends1.put(usid, usid);
+
+                fStore.collection("users").document(usid).collection("friends").document(otherid).set(friends, SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Toast.makeText(Beanchrichtigung.this,"Ihr seid jetzt Freunde", Toast.LENGTH_SHORT).show();
+                        fStore.collection("users").document(otherid).collection("request").document(otherid +usid).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Beanchrichtigung.this, "Nö", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                        fStore.collection("users").document(usid).collection("request").document(usid+ otherid).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Beanchrichtigung.this, "Nö", Toast.LENGTH_SHORT).show();
+
+                            }
+                        });
+                    }
+                });
+                fStore.collection("users").document(otherid).collection("friends").document(usid).set(friends1,SetOptions.merge()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                    }
+                });
+            }
+
+        });
 
 
     }
