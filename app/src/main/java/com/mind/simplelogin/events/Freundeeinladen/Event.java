4c6b394ee016326e2ecab39e3d9736ddfc0a1a1f
stelz <stelzer.l@icloud.com>
2020-04-08 18:18:14 +0200

    Eventeinladungen laufen

---
+++ app/src/main/java/com/mind/simplelogin/events/Freundeeinladen/Event.java
@@ -0,0 +1,62 @@
+package com.mind.simplelogin.events.Freundeeinladen;
+
+import com.mind.simplelogin.events.Freundeeinladen.Eventid;
+
+
+public class Event extends Eventid {
+    public Event(String Eventname, String Ort, String Teilnehmer, String Zeit, String Datum){
+            this.Eventname=Eventname;
+            this.Ort=Ort;
+            this.Teilnehmer=Teilnehmer;
+            this.Zeit=Zeit;
+            this.Datum=Datum;
+    }
+    public Event(){
+    }
+
+    public String getEventname() {
+        return Eventname;
+    }
+
+    public void setEventname(String eventname) {
+        Eventname = eventname;
+    }
+
+    public String getOrt() {
+        return Ort;
+    }
+
+    public void setOrt(String ort) {
+        Ort = ort;
+    }
+
+    public String getTeilnehmer() {
+        return Teilnehmer;
+    }
+
+    public void setTeilnehmer(String teilnehmer) {
+        Teilnehmer = teilnehmer;
+    }
+
+    public String getZeit() {
+        return Zeit;
+    }
+
+    public void setZeit(String zeit) {
+        Zeit = zeit;
+    }
+
+    public String getDatum() {
+        return Datum;
+    }
+
+    public void setDatum(String datum) {
+        Datum = datum;
+    }
+
+    String Eventname;
+    String Ort;
+    String Teilnehmer;
+    String Zeit;
+    String Datum;
+}
